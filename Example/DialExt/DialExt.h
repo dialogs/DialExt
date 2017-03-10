c44d8b106e2c2a1aa808f38689557ca5964b45c3
Vladlex <vladlexion@gmail.com>
2017-03-10 12:20:43 +0300

    .ignore fix header file

---
+++ Example/DialExt/DialExt.h
@@ -0,0 +1,19 @@
+//
+//  DialExt.h
+//  DialExt
+//
+//  Created by Aleksei Gordeev on 10/03/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DialExt.
+FOUNDATION_EXPORT double DialExtVersionNumber;
+
+//! Project version string for DialExt.
+FOUNDATION_EXPORT const unsigned char DialExtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DialExt/PublicHeader.h>
+
+
