80577a4b12a05921cc5038a40ac1edf0f1b76f8a
Vladlex <vladlexion@gmail.com>
2017-04-19 18:30:34 +0300

    Add tests and some basic helping functions

---
+++ Example/DialExt/Code/Extensions/Bundle+DialExt.swift
@@ -0,0 +1,30 @@
+//
+//  Bundle+DialExt.swift
+//  DialExt
+//
+//  Created by Aleksei Gordeev on 19/04/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    
+    func allResources() -> [URL] {
+        // Enumerators are recursive
+        guard let enumerator = FileManager.default.enumerator(atPath: self.bundlePath) else {
+            return []
+        }
+        
+        var filePaths: [URL] = []
+        
+        while let object = enumerator.nextObject() {
+            if let path = object as? String {
+                filePaths.append(bundleURL.appendingPathComponent(path))
+            }
+        }
+        
+        return filePaths
+    }
+    
+}
---
+++ Example/DialExt/Code/Extensions/SwiftMisc.swift
@@ -0,0 +1,35 @@
+//
+//  SwiftMisc.swift
+//  DialExt
+//
+//  Created by Aleksei Gordeev on 19/04/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+// Extending Misc.swift from Swift library
+
+/// Performs block only if instance is not nil.
+public func withOptionalExtendedLifetime<T, Result>(_ x: T?,
+                                     _ ifNilResult: Result? = nil,
+                                     _ body:() throws -> Result) rethrows -> Result? {
+    return try withExtendedLifetime(x, {
+        guard x != nil else {
+            return ifNilResult
+        }
+        return try body()
+    })
+}
+
+/// Performs block only if instance is not nil. Does not pass instance into performing block.
+public func withOptionalExtendedLifetime<T, Result>(_ x: T?,
+                                     _ ifNilResult: Result? = nil,
+                                     _ body: (T) throws -> Result) rethrows -> Result? {
+    return try withExtendedLifetime(x, {
+        guard let notNilX = x else {
+            return ifNilResult
+        }
+        return try body(notNilX)
+    })
+}
