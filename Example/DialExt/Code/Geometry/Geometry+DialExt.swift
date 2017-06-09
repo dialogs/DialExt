34f6aeb71727ef0e6b34ea4a586d2ae046b9ff65
Vladlex <vladlexion@gmail.com>
2017-06-09 19:35:51 +0300

    Add Geometry helpers

---
+++ Example/DialExt/Code/Geometry/Geometry+DialExt.swift
@@ -0,0 +1,156 @@
+//
+//  Geometry+DialExt.swift
+//  Pods
+//
+//  Created by Aleksei Gordeev on 09/06/2017.
+//
+//
+
+import Foundation
+
+extension CGRect {
+    
+    /// Change size by applying new maxX
+    public mutating func resize(maxX: CGFloat) {
+        self = self.resized(maxX: maxX)
+    }
+    
+    public func resized(maxX: CGFloat) -> CGRect {
+        let difference = maxX - self.maxX
+        guard difference < 0.0 else {
+            return self
+        }
+        return CGRect.init(origin: self.origin,
+                           size: CGSize(width: self.size.width + difference, height: self.size.height))
+    }
+    
+    public func adjusting(insets: UIEdgeInsets) -> CGRect {
+        return UIEdgeInsetsInsetRect(self, insets)
+    }
+    
+    public func originX(centeredWidth: CGFloat) -> CGFloat {
+        return self.minX + (self.size.width - centeredWidth) / 2.0
+    }
+    
+    public func originY(centeredHeight: CGFloat) -> CGFloat {
+        return self.minY + (self.size.height - centeredHeight) / 2.0
+    }
+    
+    public var topHalfRect: CGRect {
+        return CGRect(origin: self.origin, size: CGSize(width: self.size.width, height: self.size.height / 2.0))
+    }
+    
+    public var bottomHalfRect: CGRect {
+        return self.adjusting(insets: .init(top: self.size.height / 2.0, left: 0.0, bottom: 0.0, right: 0.0))
+    }
+    
+    public func verticalPositionedSubrects(count: Int) -> [CGRect] {
+        guard count > 1 else {
+            return [self]
+        }
+        var rects: [CGRect] = []
+        let height = self.size.height / CGFloat(count)
+        for idx in 0..<count {
+            let origin = CGPoint(x: self.minX, y: self.minY + CGFloat(idx) * height)
+            let rect = CGRect(origin: origin, size: CGSize(width: self.size.width, height: height))
+            rects.append(rect)
+        }
+        return rects
+    }
+    
+    public func subrect(size: CGSize, position: Position = .none, alignment: CenterAlignment = .all, limited: Bool = true) -> CGRect {
+        
+        let rectToPlace: CGRect
+        
+        var targetSize = size
+        
+        switch position {
+        case .topHalf: rectToPlace = self.topHalfRect
+        case .bottomHalf: rectToPlace = self.bottomHalfRect
+        case .none: rectToPlace = self
+        }
+        
+        if limited {
+            targetSize = CGSize(width: min(targetSize.width, rectToPlace.size.width),
+                                height: min(targetSize.height, rectToPlace.size.height))
+        }
+        
+        var origin = self.origin
+        if alignment.contains(.horizontal) {
+            origin.x = rectToPlace.originX(centeredWidth: targetSize.width)
+        }
+        if alignment.contains(.vertical) {
+            origin.y = rectToPlace.originY(centeredHeight: targetSize.height)
+        }
+        
+        return CGRect(origin: origin, size: targetSize)
+    }
+    
+    public enum Position: Int {
+        
+        /**
+         Positioning element supposed to be placed just in the center of rect
+         */
+        case none
+        
+        
+        /**
+         Positioning element supposed to be placed in top half aread
+         
+         ```
+         ------------
+         |          |
+         | element  |   - Top half.
+         |          |
+         ------------
+         |          |
+         |          |   - Bottom half
+         |          |
+         ------------
+         ```
+         */
+        case topHalf
+        
+        /**
+         Positioning element supposed to be placed in top half aread
+         
+         ```
+         ------------
+         |          |
+         |          |   - Top half.
+         |          |
+         ------------
+         |          |
+         | element  |   - Bottom half
+         |          |
+         ------------
+         ```
+         */
+        case bottomHalf
+    }
+    
+    
+    public struct CenterAlignment: OptionSet {
+        
+        public let rawValue: Int
+
+        public typealias RawValue = Int
+        
+        public init(rawValue: RawValue) {
+            self.rawValue = rawValue
+        }
+        
+        public static let horizontal = CenterAlignment(rawValue: 1 << 0)
+        
+        public static let vertical = CenterAlignment(rawValue: 1 << 1)
+        
+        public static let all: CenterAlignment = [.horizontal, .vertical]
+    }
+    
+}
+
+extension UIEdgeInsets {
+    init(horizontal: CGFloat, vertical: CGFloat) {
+        self.init(top: vertical, left: horizontal, bottom: vertical, right: horizontal)
+    }
+}
