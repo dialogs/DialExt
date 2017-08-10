056e668c503e70d065940b40281094183d548c58
Vladlex <vladlexion@gmail.com>
2017-08-10 22:16:57 +0300

    Add constraints to view

--- Example/DialExt/Code/Extensions/NSLayoutConstraint+DialExt.swift
+++ Example/DialExt/Code/Extensions/NSLayoutConstraint+DialExt.swift
@@ -10,7 +10,10 @@ import Foundation
 
 extension NSLayoutConstraint {
     
-    public class func de_wrappingView(_ view: UIView, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint] {
+    public class func de_wrappingView(_ view: UIView,
+                                      bindToTopLayoutGuide: UILayoutSupport? = nil,
+                                      bindToBottomLayoutGuide: UILayoutSupport? = nil,
+                                      insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint] {
         
         let viewName = "view"
         let viewsInfo = [viewName: view]
@@ -23,9 +26,41 @@ extension NSLayoutConstraint {
         
         let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|-left-[\(viewName)]-right-|", options: [], metrics: metrics, views: viewsInfo)
         
-        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[\(viewName)]-bottom-|", options: [], metrics: metrics, views: viewsInfo)
+        var verticalConstraints: [NSLayoutConstraint] = []
+        if let guide = bindToTopLayoutGuide {
+            let constraint = NSLayoutConstraint.init(item: view, attribute: .top, relatedBy: .equal,
+                                                     toItem: guide, attribute: .bottom, multiplier: 1.0,
+                                                     constant: insets.top)
+            verticalConstraints.append(constraint)
+        }
+        else {
+            let constraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[\(viewName)]",
+                metrics: metrics,
+                views: viewsInfo)
+            verticalConstraints.append(contentsOf: constraints)
+        }
+        
+        if let guide = bindToBottomLayoutGuide {
+            let constraint = NSLayoutConstraint.init(item: view, attribute: .bottom, relatedBy: .equal,
+                                                     toItem: guide, attribute: .top, multiplier: 1.0,
+                                                     constant: insets.bottom)
+            verticalConstraints.append(constraint)
+        }
+        else {
+            let constraints = NSLayoutConstraint.constraints(withVisualFormat: "V:[\(viewName)]-bottom-|",
+                metrics: metrics,
+                views: viewsInfo)
+            verticalConstraints.append(contentsOf: constraints)
+        }
+        
+        
         let constraints = horizontalConstraints + verticalConstraints
         return constraints
     }
-    
+}
+
+public extension Array where Element == NSLayoutConstraint {
+    public func activate() {
+        NSLayoutConstraint.activate(self)
+    }
 }
