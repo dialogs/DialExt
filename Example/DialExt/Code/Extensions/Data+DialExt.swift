cbbf93ef17a8b2530532fa89efd289b736d89608
Vladlex <vladlexion@gmail.com>
2017-08-10 13:43:28 +0300

    Add Crc, remove MD5 (Crypto framework is partially unavailable for extension)

--- Example/DialExt/Code/Extensions/Data+DialExt.swift
+++ Example/DialExt/Code/Extensions/Data+DialExt.swift
@@ -8,24 +8,16 @@
 
 import Foundation
 
+
 public extension Data {
     
     public var hexString: String {
         return self.map({String(format: "%02hhx", $0)}).joined()
     }
     
-    // TODO: move to DialExt
-    var md5: Data {
-        var result = Data(count: Int(CC_MD5_DIGEST_LENGTH))
-        _ = result.withUnsafeMutableBytes {resultPtr in
-            self.withUnsafeBytes {(bytes: UnsafePointer<UInt8>) in
-                CC_MD5(bytes, CC_LONG(count), resultPtr)
-            }
-        }
-        return result
+    var crc32: UInt32 {
+        let crc = CRC32.init(data: self)
+        return crc.crc
     }
     
-    var md5String: String {
-        return self.md5.binaryString
-    }
 }
